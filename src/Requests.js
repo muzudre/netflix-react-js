4d0497d5c88cd2515ae966841ae906bb2ba98943
Muslim Zabirov <muzudre@gmail.com>
2022-11-16 11:59:21 +0500

    finish the main component

---
+++ src/Requests.js
@@ -0,0 +1,11 @@
+const key = 'b8612eff37f271ab492393b5d55a2e00'
+
+const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`
+}
+
+export default requests;
