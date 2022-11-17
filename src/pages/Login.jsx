d83b46cea37f7053a3d499bc2e97ff243111d3ff
Muslim Zabirov <muzudre@gmail.com>
2022-11-17 09:28:11 +0500

    complet protected route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
 
 const Login = () => {
+  const { user, logIn } = UserAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="w-full h-screen">
       <img
@@ -14,14 +33,17 @@ const Login = () => {
         <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
           <div className="max-w-[320px] mx-auto py-16">
             <h1 className="text-3xl font-bold">Sign In</h1>
-            <form className="flex w-full flex-col py-4">
+            {error ? <p className="p-3 bg-red-400 my-3">{error}</p> : null}
+            <form onSubmit={handleSubmit} className="flex w-full flex-col py-4">
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 className="p-3 my-2 bg-gray-700 rounded"
                 type="email"
                 placeholder="Email"
                 autoComplete="email"
               />
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 className="p-3 my-2 bg-gray-700 rounded"
                 type="password"
                 placeholder="Password"
